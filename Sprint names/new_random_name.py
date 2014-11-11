a73f35ee283b43a7418803c127438c74704a01ee
ToxaZ <anton.zainiev@gmail.com>
2014-11-11 21:32:31 +0300

    easygui changed to wxPython
    cyrillic names still don't displayed correctly

--- Sprint names/new_random_name.py
+++ Sprint names/new_random_name.py
@@ -4,7 +4,8 @@ __author__ = 'Антон Зайниев'
 import urllib2
 import pandas
 import random
-import easygui
+import wx
+import codecs
 
 
 theurl = 'http://pubs.zvq.me/top_slices/latest/general_top_tracks.csv'
@@ -34,25 +35,36 @@ top100_table = urllib2.urlopen(theurl)
 # authentication is now handled automatically for us
 
 colnames = ["cnt","track","release","artist_1", "artist_2", "artist_3"]
-data = pandas.read_csv(top100_table, names=colnames, quotechar='"', delimiter=',', skipinitialspace=True, encoding='utf-8')
-artists = list(data.artist_1)
+data = pandas.read_csv(top100_table, names=colnames, skipinitialspace=True)
 dec_artists = []
-for i in artists:
-    j = i.encode('utf-8')
-    dec_artists.append(j)
-set_artists = set(artists)
+artists = list(data.artist_1) + list(data.artist_2) + list(data.artist_3)
+for a in artists:
+    try:
+        b = a.strip('[]"')
+        dec_artists.append(b)
+    except:
+        continue
+set_artists = set(dec_artists)
 
 choice = False
-artists_check = open('sprint_names.txt', 'r')
+artists_check = codecs.open('sprint_names.txt', 'r')
+sprint_name = str(random.sample(set_artists, 1))
+sprint_name = sprint_name.strip("'[]")
+dec_sprint_name = sprint_name.decode('utf8')
 for l in artists_check:
     if l == sprint_name:
         sprint_name = str(random.sample(set_artists, 1))
         continue
     while choice == False:
-        if easygui.ccbox('RNG suggests ' + str(sprint_name), 'Confirm'):
+        app = wx.App(False)
+        dlg = wx.MessageDialog(None, "RNG suggests " + sprint_name, 'Confirm', wx.YES_NO | wx.ICON_INFORMATION)
+        retCode = dlg.ShowModal()
+        if (retCode == wx.ID_YES):
             artists_check = open('sprint_names.txt', 'a')
             artists_check.write(str(sprint_name) + ', ')
-            print "You chose " + (u''.join(sprint_name))
+            print "You chose " + sprint_name
             choice = True
+            dlg.Destroy()
         else:
+            dlg.Destroy()
             break
