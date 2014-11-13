47840dacb2166c2750a3ba3d321b249bbcdbae82
ToxaZ <anton.zainiev@gmail.com>
2014-11-13 19:38:00 +0300

    Back to easygui. Cyrillic names error fixed. Comments added.

--- Sprint names/new_random_name.py
+++ Sprint names/new_random_name.py
@@ -4,67 +4,59 @@ __author__ = 'Антон Зайниев'
 import urllib2
 import pandas
 import random
-import wx
-import codecs
+import easygui
 
-
+# attributes to log on pubs.zvq.me
 theurl = 'http://pubs.zvq.me/top_slices/latest/general_top_tracks.csv'
 username = 'pubs'
 password = 'sei4Iof0Aem0eaH2'
 
-
+# have no idea how, but it works
 passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-# this creates a password manager
 passman.add_password(None, theurl, username, password)
-# because we have put None at the start it will always
-# use this username/password combination for  urls
-# for which `theurl` is a super-url
-
 authhandler = urllib2.HTTPBasicAuthHandler(passman)
-# create the AuthHandler
-
 opener = urllib2.build_opener(authhandler)
-
 urllib2.install_opener(opener)
-# All calls to urllib2.urlopen will now use our handler
-# Make sure not to include the protocol in with the URL, or
-# HTTPPasswordMgrWithDefaultRealm will be very confused.
-# You must (of course) use it when fetching the page though.
 
+# openings .csv, creating list of artists
 top100_table = urllib2.urlopen(theurl)
-# authentication is now handled automatically for us
-
 colnames = ["cnt","track","release","artist_1", "artist_2", "artist_3"]
 data = pandas.read_csv(top100_table, names=colnames, skipinitialspace=True)
-dec_artists = []
 artists = list(data.artist_1) + list(data.artist_2) + list(data.artist_3)
-for a in artists:
-    try:
-        b = a.strip('[]"')
-        dec_artists.append(b)
-    except:
-        continue
-set_artists = set(dec_artists)
 
-choice = False
-artists_check = codecs.open('sprint_names.txt', 'r')
-sprint_name = str(random.sample(set_artists, 1))
+# clearing names
+set_artists = []
+for a in data.artist_1:
+    b = a.strip('[]"')
+    set_artists.append(b)
+
+# clearing duplicates
+set_artists = set(set_artists)
+set_artists = list(set_artists)
+
+# choosing sprint name
+r = random.randint(0,len(set_artists) - 1)
+sprint_name = set_artists[r]
 sprint_name = sprint_name.strip("'[]")
-dec_sprint_name = sprint_name.decode('utf8')
+
+# checking if it hasn't been used before (sprint_names.csv contains previously used sprint names), creating new one if needed
+check_names = ['artist']
+data_check = pandas.read_csv("sprint_names.csv", names = check_names)
+artists_check = data_check['artist'].values.tolist()
 for l in artists_check:
-    if l == sprint_name:
-        sprint_name = str(random.sample(set_artists, 1))
+    if str(l) == str(sprint_name):
+        r = random.randint(0,len(set_artists))
+        sprint_name = set_artists[r]
+        sprint_name = sprint_name.strip("'[]")
         continue
-    while choice == False:
-        app = wx.App(False)
-        dlg = wx.MessageDialog(None, "RNG suggests " + sprint_name, 'Confirm', wx.YES_NO | wx.ICON_INFORMATION)
-        retCode = dlg.ShowModal()
-        if (retCode == wx.ID_YES):
-            artists_check = open('sprint_names.txt', 'a')
-            artists_check.write(str(sprint_name) + ', ')
-            print "You chose " + sprint_name
-            choice = True
-            dlg.Destroy()
-        else:
-            dlg.Destroy()
-            break
+
+# requesting confirmation; if confirmed appending sprint name in sprint_names.csv
+choice = False
+while choice == False:
+    if easygui.ccbox('RNG suggests ' + str(sprint_name), 'Confirm'):
+        print ("You chose " + str(sprint_name))
+        choice = True
+        data_check.loc[len(data_check) + 1] = sprint_name
+        data_check.to_csv("sprint_names.csv", header = False)
+    else:
+        break
