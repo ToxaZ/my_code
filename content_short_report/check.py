010d7a37f9c37cb7ad1053cb6414ba2f1ee86cf0
ToxaZ <anton.zainiev@gmail.com>
2014-12-19 17:17:28 +0300

    Source type added to "listen" function in short report

--- content_short_report/check.py
+++ content_short_report/check.py
@@ -17,21 +17,25 @@ def id_split(id):
     except:
         return "Please provide space delimited sequence"
 
-def listen(id):
+def listen(id, listen_type):
     """Returning the number of playevents longer than 30 sec with ok flag for certain ids"""
+
+    # testing if purchase type is correct
+    if listen_type not in ('release', 'track', 'playlist'):
+        raise AttributeError("provide valid listen_type: 'release', 'playlist' or 'track'")
 
     #splitting ids
     cid = id_split(id)
 
     # uncomment the line below to print created Hive query
-    # print ('select count(1), src_id from playevent where %s and play_duration > 30 and ok_flag group by src_id;' % (cid))
+    # print ("select count(1), src_id, src_type from playevent where (%s) and src_type = '%s' and play_duration > 30 and ok_flag group by src_id, src_type" % (cid, listen_type))
 
     # connecting to Hive, sending query, returning results of query
     conn = pyhs2.connect(host='nif-nif.zvq.me', port=10000, authMechanism="NOSASL", user='hive', password='test',
                          database='default')
     cur = conn.cursor()
     cur.execute(
-        'select count(1), src_id from playevent where %s and play_duration > 30 and ok_flag group by src_id' % (cid))
+        "select count(1), src_id, src_type from playevent where (%s) and src_type = '%s' and play_duration > 30 and ok_flag group by src_id, src_type" % (cid, listen_type))
     results = cur.fetch()
     cur.close()
     conn.close()
@@ -50,16 +54,19 @@ def purchase(id, purchase_type):
 
     #splitting ids
     cid = id_split(id)
+    cid = cid.replace('src_id', ctype)
 
     # uncomment the line below to print created Hive query
-    # print ('select count(transaction_id), %s from purchase where %s group by %s;' % (ctype, cid, ctype))
+    # print ('select count(transaction_id), %s from purchase where (%s) group by %s' % (ctype, cid, ctype))
 
     # connecting to Hive, sending query, returning results of query
     conn = pyhs2.connect(host='nif-nif.zvq.me', port=10000, authMechanism="NOSASL", user='hive', password='test',
                          database='default')
     cur = conn.cursor()
-    cur.execute('select count(transaction_id), %s from purchase where %s group by %s' % (ctype, cid, ctype))
+    cur.execute('select count(transaction_id), %s from purchase where (%s) group by %s' % (ctype, cid, ctype))
     results = cur.fetch()
     cur.close()
     conn.close()
     return results
+
+purchase('2649573 2654782','release')
