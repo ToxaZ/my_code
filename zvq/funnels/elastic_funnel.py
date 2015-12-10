e64edbf33d960295f95fc2995a9587eefe724dc5
ToxaZ <az@dreamindustries.co>
2015-12-10 21:36:28 +0300

    adding some job scripts

---
+++ zvq/funnels/elastic_funnel.py
@@ -0,0 +1,117 @@
+__author__ = 'ToxaZ'
+
+from pyanalytics.kpi2.metrics.conversion_utils.processor import time_based_funnel
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('elasticsearch').setLevel(logging.INFO)
+
+app_opened_query = {
+    "type": "elastic",
+    "q": "NOT properties.app: fonoteka",
+    "index": "cs-app_opened*",
+    "agg_field": "app_instance"
+    }
+
+install_vmetro_query = {
+    "type": "elastic",
+    "q": "properties.sub_site: vmetro",
+    "index": "cs-mat_app_install*",
+    "agg_field": "app_instance"
+    }
+
+active_query_ai = {
+    "type": "elastic",
+    "q": "NOT properties.app: fonoteka",
+    "index": "cs-track_playback*",
+    "agg_field": "app_instance"
+    }
+
+active_query = {
+    "type": "elastic",
+    "q": "mat_info.sub_site:vmetro",
+    "index": "cs-track_playback*"
+    }
+
+app_open_vmetro_query = {
+    "type": "elastic",
+    "q": "mat_info.sub_site:vmetro",
+    "index": "cs-app_opened*"
+    }
+
+subscr_query = {
+    "type": "psql",
+    "q": "select user_id from app_subscription "
+    "where is_trial = false and price > 0 and app != 'fonoteka'"
+    }    
+
+genres_shown_query = {
+    "type": "elastic",
+    "q": "features: NEW_GENRE_PAGE",
+    "index": "cs-main_page_genres_shown*"
+}
+
+genres_clicked_query = {
+    "type": "elastic",
+    "q": "features: NEW_GENRE_PAGE AND properties.url: *\\/genre*",
+    "index": "cs-genres_page_clicked*",
+    "index": "cs-global_play*"
+}
+
+web_users_query = {
+    "type": "elastic",
+    "q": "properties.app:web AND NOT properties.utm-tags.utm_source:topt",
+    "index": "cs-*"
+}
+
+active_10m_query = {
+    "type": "hive",
+    "q": "select user_id from playevent group by user_id having sum(play_duration) >= 600"
+}
+
+mobile_pop_up_query = {
+    "type": "elastic",
+    "q": "properties.type: pop-up",
+    "index": "cs-mobile_shown*"
+}
+
+mobile_pop_up_action_query = {
+    "type": "elastic",
+    "q": "properties.type: pop-up",
+    "index": "cs-mobile_link_requested*", 
+    "index": "cs-mobile_install_clicked*"
+}
+
+app_open_version_query = {
+    "type": "elastic",
+    "q": "properties.os: android AND properties.app_release: 1.9*",
+    "index": "cs-app_opened*",
+    "time_unit": "year",
+    "agg_field": "app_instance"
+}
+
+app_inst_registration_query = {
+    "type": "elastic",
+    "q": "*",
+    "index": "cs-authentication_successful*",
+    "agg_field": "app_instance"
+}
+
+app_registration_query = {
+    "type": "elastic",
+    "q": "properties.os: android AND properties.app_release: 1.10*",
+    "index": "cs-authentication_successful*",
+}
+
+app_trial_query = {
+    "type": "psql",
+    "q": "select user_id from app_subscription "
+    "where is_trial = true and app != 'fonoteka'"
+}
+
+# print (time_based_funnel(app_opened_query, [active_query_ai]))
+print (time_based_funnel(app_open_vmetro_query, []))
+# print(time_based_funnel(web_users_query, [genres_shown_query, genres_clicked_query, active_10m_query])) # ZA-420
+# print(time_based_funnel(web_users_query, [mobile_pop_up_query, mobile_pop_up_action_query])) # ZA-419
+# print(time_based_funnel(app_open_version_query, [app_inst_registration_query])) # ZA-469
+# print(time_based_funnel(app_registration_query, [app_trial_query])) # ZA-469
