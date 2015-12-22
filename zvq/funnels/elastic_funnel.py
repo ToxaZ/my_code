db65b7a71fd24e295679329169935c3f4c6d958b
ToxaZ <az@dreamindustries.co>
2015-12-22 21:35:13 +0300

    funnel tool almost done

--- zvq/funnels/elastic_funnel.py
+++ zvq/funnels/elastic_funnel.py
@@ -2,185 +2,41 @@ __author__ = 'ToxaZ'
 
 from pyanalytics.kpi2.metrics.conversion_utils.processor import funnel, time_based_funnel
 import logging
-import inspect
+import sys
+import yaml
 import pandas as pd
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logging.getLogger('elasticsearch').setLevel(logging.WARNING)
 
-app_opened_query_android = {
-    "type": "elastic",
-    "q": "NOT properties.app: fonoteka AND properties.os: android",
-    "time_unit": "month",
-    "index": "cs-app_opened*"
-    }
-
-app_opened_query_ios = {
-    "type": "elastic",
-    "q": "NOT properties.app: fonoteka AND properties.os: iOS",
-    "index": "cs-app_opened*",
-    "agg_field": "app_instance"
-    }
-
-install_vmetro_query = {
-    "type": "elastic",
-    "q": "properties.sub_site: vmetro",
-    "index": "cs-mat_app_install*",
-    "agg_field": "app_instance"
-    }
-
-install_ios_query = {
-    "type": "elastic",
-    "q": "properties.mat_app: 'Zvooq iOS'",
-    "index": "cs-mat_app_install-2015.1*",
-    "agg_field": "app_instance"
-    }
-
-active_query_ai = {
-    "type": "elastic",
-    "q": "NOT properties.app: fonoteka",
-    "index": "cs-track_playback*",
-    "agg_field": "app_instance"
-    }
-
-active_query_android = {
-    "type": "elastic",
-    "q": "NOT properties.app: fonoteka AND  properties.phoneType: android",
-    "index": "cs-playevent*"
-    }
-
-active_query_ios = {
-    "type": "elastic",
-    "q": "NOT properties.app: fonoteka AND  properties.phoneType: ios",
-    "index": "cs-playevent*"
-    }
-
-app_open_vmetro_query = {
-    "type": "elastic",
-    "q": "mat_info.sub_site:vmetro",
-    "index": "cs-app_opened*"
-    }
-
-subscr_query = {
-    "type": "psql",
-    "q": "select user_id from app_subscription "
-    "where is_trial = false and price > 0 and app != 'fonoteka'"
-    }
-
-genres_shown_query = {
-    "type": "elastic",
-    "q": "features: NEW_GENRE_PAGE",
-    "index": "cs-main_page_genres_shown*"
-}
-
-genres_clicked_query = {
-    "type": "elastic",
-    "q": "features: NEW_GENRE_PAGE AND properties.url: *\\/genre*",
-    "index": "cs-genres_page_clicked*",
-    "index": "cs-global_play*"
-}
-
-web_users_query = {
-    "type": "elastic",
-    "q": "properties.app:web AND NOT properties.utm-tags.utm_source:topt",
-    "index": "cs-*"
-}
-
-web_users_query_agg_by_cookie = {
-    "type": "elastic",
-    "q": "properties.app:web AND NOT properties.utm-tags.utm_source:topt",
-    "index": "cs-*",
-    "time_unit": "month",
-    "agg_field": "properties.user_cookie"
-}
+json_ex = {'2015-05-01': [3787, 2128, 128], '2015-10-01': [59716, 42404, 426], '2015-03-01': [9713, 6018, 357], '2015-12-01': [20636, 4781, 66], '2014-12-01': [5, 5, 4], '2015-08-01': [15634, 11025, 184], '2015-02-01': [15, 11, 7], '2015-06-01': [4134, 2463, 108], '2015-09-01': [40797, 30107, 210], '2015-04-01': [5234, 3046, 189], '2014-11-01': [3, 2, 1], '2015-11-01': [51560, 35621, 292], '2015-07-01': [6164, 3449, 109], '2015-01-01': [1, 1, 1]}
 
 
-active_10m_query = {
-    "type": "hive",
-    "q": "select user_id from playevent group by user_id"
-    "having sum(play_duration) >= 600"
-}
+def get_queries():
+    queries = []
+    with open('query.yaml', 'r') as f:
+        doc = yaml.load_all(f)
+        for d in doc:
+            queries.append(d.copy())
+    return queries
 
-mobile_pop_up_query = {
-    "type": "elastic",
-    "q": "properties.type: pop-up",
-    "index": "cs-mobile_shown*"
-}
 
-mobile_pop_up_action_query = {
-    "type": "elastic",
-    "q": "properties.type: pop-up",
-    "index": "cs-mobile_link_requested*",
-    "index": "cs-mobile_install_clicked*"
-}
+def make_request(time_based=True):
+    first_query_step_funnel = {}
+    queries = []
+    for funnel_step in sys.argv[1:]:
+        for q in get_queries():
+            if funnel_step == q['name']:
+                if q['name'] == sys.argv[1]:
+                    first_query_step_funnel = q['query']
+                else:
+                    queries.append(q['query'].copy())
+    return time_based_funnel(first_query_step_funnel, queries)
 
-app_open_version_query = {
-    "type": "elastic",
-    "q": "properties.os: android AND properties.app_release: 1.9*",
-    "index": "cs-app_opened*",
-    "time_unit": "year",
-    "agg_field": "app_instance"
-}
 
-registration_query_agg_by_app_instance = {
-    "type": "elastic",
-    "q": "*",
-    "index": "cs-authentication_successful*",
-    "agg_field": "app_instance"
-}
+def results_to_dataframe(funnel_response):
+    df = pd.DataFrame(funnel_response, index=sys.argv[1:])
+    return df.transpose()
 
-registration_query_agg_by_user_cookie = {
-    "type": "elastic",
-    "q": "*",
-    "index": "cs-authentication_successful*",
-    "time_unit": "month",
-    "agg_field": "roperties.user_cookie"
-}
 
-app_registration_query_elastic = {
-    "type": "elastic",
-    "q": "properties.os: android AND properties.app_release: 1.10*",
-    "index": "cs-authentication_successful*",
-}
-
-time_based_trial_query = {
-    "type": "psql",
-    "q": "select user_id, added from app_subscription "
-    "where is_trial = true and app != 'fonoteka'"
-}
-
-time_based_subscription_query = {
-    "type": "psql",
-    "q": "select user_id, added from app_subscription "
-    "where is_trial = false and app != 'fonoteka'"
-}
-
-# print (time_based_funnel(app_opened_query, [active_query_ai]))
-# print (funnel(
-#     web_users_query_agg_by_cookie,
-#     [registration_query_agg_by_user_cookie]
-#     )
-# )  # ZA-418
-# print(time_based_funnel(web_users_query, [genres_shown_query, genres_clicked_query, active_10m_query])) # ZA-420
-# print(time_based_funnel(web_users_query, [mobile_pop_up_query, mobile_pop_up_action_query])) # ZA-419
-# print(time_based_funnel(app_open_version_query, [app_inst_registration_query])) # ZA-469
-# print(time_based_funnel(app_registration_query, [app_trial_query])) # ZA-469
-# print funnel([app_opened_query_ios, active_query_ios], print_query=True) # ZA-537
-print(
-    pandas.read_json(
-        time_based_funnel(
-            app_opened_query_ios,
-            [time_based_trial_query, time_based_subscription_query]
-        )
-    )
-)  # ZA-537
-# print(time_based_funnel(
-#     app_opened_query_android,
-#     [active_query_android]
-#     )
-# )  # ZA-537
-# print(time_based_funnel(
-#     app_opened_query_ios,
-#     [active_query_ios]
-#     )
-# )  # ZA-537
+print make_request()
