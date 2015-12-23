8c9618c83ee0f0acadca148f5f6e6fdd62ea09b2
ToxaZ <az@dreamindustries.co>
2015-12-23 20:39:25 +0300

    small refactoring

--- zvq/funnels/elastic_funnel.py
+++ zvq/funnels/elastic_funnel.py
@@ -9,37 +9,27 @@ import pandas as pd
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('elasticsearch').setLevel(logging.WARNING)
 
+
 def get_queries():
-    queries = []
     with open('query.yaml', 'r') as f:
-        doc = yaml.load_all(f)
-        for d in doc:
-            queries.append(d.copy())
-    return queries
+        doc_queries = yaml.load(f)
+    return doc_queries
 
 
 def make_request(time_based=True):
     first_query_step_funnel = {}
     queries = []
+    funnel_query = map(lambda x: get_queries().get(x), sys.argv[1:])
+
     if time_based is True:
-        for funnel_step in sys.argv[1:]:
-            for q in get_queries():
-                if funnel_step == q['name']:
-                    if q['name'] == sys.argv[1]:
-                        first_query_step_funnel = q['query']
-                    else:
-                        queries.append(q['query'].copy())
-        return time_based_funnel(first_query_step_funnel, queries)
+        return time_based_funnel(funnel_query[0], funnel_query[1:])
     if time_based is False:
-        for funnel_step in sys.argv[1:]:
-            for q in get_queries():
-                queries.append(q['query'].copy())
-        return funnel(queries)
+        return funnel(funnel_query)
 
 
-def df_results(funnel_response):
+def dataframe_results(funnel_response):
     df = pd.DataFrame(funnel_response, index=sys.argv[1:])
     return df.transpose()
 
 
-print df_results(make_request())
+print dataframe_results(make_request())
